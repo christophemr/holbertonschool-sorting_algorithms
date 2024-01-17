f0165d50bc188928154497678fdec57ab2f14d91
christophemr <christophemr59@gmail.com>
2024-01-17 09:49:00 +0100

    documentation & new line added

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,11 @@
 #include "sort.h"
 
-/***/
+/**
+ * insertion_sort_list - sorts a doubly linked list of
+ * integers using the insertion sort list algorithm
+ * @list: doubly linked list to be sorted
+ */
+
 void insertion_sort_list(listint_t **list)
 {
 	listint_t	*current;
@@ -37,3 +42,4 @@ void insertion_sort_list(listint_t **list)
 		current = current->next;
 	}
 }
+
