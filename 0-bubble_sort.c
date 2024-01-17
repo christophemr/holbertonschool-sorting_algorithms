3fdb50b129dbd4c915a1f45e32bd52bfd4ea4d93
christophemr <christophemr59@gmail.com>
2024-01-17 09:35:25 +0100

    swap function defined in bubble_sort function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,20 @@
 #include "sort.h"
 
 /**
+ * swap - swap to values
+ * @a: first value to swap
+ * @b: second value to swap
+ */
+
+void swap(int *a, int *b)
+{
+        int tmp;
+
+        tmp = *b;
+        *b = *a;
+        *a = tmp;
+}
+/**
  * bubble_sort - sorts an array using the bubble sort algorithm
  * @array: array to be sorted
  * @size: size of the array
@@ -9,9 +23,12 @@
 void bubble_sort(int *array, size_t size)
 {
 	unsigned int i, j;
+	int flag;
+
 	/*outer loop iterates over the entire array*/
 	for (i = 0; i < size - 1; i++)
 	{
+		flag = 0;
 		/*inner loop iterates over the unsorted part of array*/
 		for (j = 0; j < size - i - 1; j++)
 		{
@@ -20,9 +37,10 @@ void bubble_sort(int *array, size_t size)
 			{
 				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
+				flag = 1;
 			}
 		}
+		if (flag == 0)
+			break;
 	}
-
-
 }
